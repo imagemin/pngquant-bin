f242006038e13dc53eaacc3faeb854daf19f7a95
1000ch <shogosensui@gmail.com>
2020-06-01 21:57:30 +0900

    Require Node.js 10

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
-import fs from 'fs';
-import path from 'path';
-import test from 'ava';
-import execa from 'execa';
-import tempy from 'tempy';
-import binCheck from 'bin-check';
-import binBuild from 'bin-build';
-import compareSize from 'compare-size';
-import m from '.';
+const fs = require('fs');
+const path = require('path');
+const test = require('ava');
+const execa = require('execa');
+const tempy = require('tempy');
+const binCheck = require('bin-check');
+const binBuild = require('bin-build');
+const compareSize = require('compare-size');
+const m = require('.');
 
 test('rebuild the pngquant binaries', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 
 	await binBuild.file(path.resolve(__dirname, 'vendor/source/pngquant.tar.gz'), [
 		'rm ./INSTALL',
-		`./configure --prefix="${tmp}"`,
-		`make install BINPREFIX="${tmp}"`
+		`./configure --prefix="${temporary}"`,
+		`make install BINPREFIX="${temporary}"`
 	]);
 
-	t.true(fs.existsSync(path.join(tmp, 'pngquant')));
+	t.true(fs.existsSync(path.join(temporary, 'pngquant')));
 });
 
 test('verify binary', async t => {
@@ -25,9 +25,9 @@ test('verify binary', async t => {
 });
 
 test('minify a png', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 	const src = path.join(__dirname, 'fixtures/test.png');
-	const dest = path.join(tmp, 'test.png');
+	const dest = path.join(temporary, 'test.png');
 	const args = [
 		'-o',
 		dest,
@@ -35,7 +35,7 @@ test('minify a png', async t => {
 	];
 
 	await execa(m, args);
-	const res = await compareSize(src, dest);
+	const result = await compareSize(src, dest);
 
-	t.true(res[dest] < res[src]);
+	t.true(result[dest] < result[src]);
 });
