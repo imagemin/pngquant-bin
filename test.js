3efc310a79307c289d36d4995227adf2fc83a712
1000ch <shogosensui@gmail.com>
2021-10-22 23:00:39 +0900

    Enable tests on Windows

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
 import execa from 'execa';
@@ -10,6 +11,12 @@ import compareSize from 'compare-size';
 import pngquant from './index.js';
 
 test('rebuild the pngquant binaries', async t => {
+	// Skip the test on Windows
+	if (process.platform === 'win32') {
+		t.pass();
+		return;
+	}
+
 	const temporary = tempy.directory();
 	const source = fileURLToPath(new URL('./vendor/source/pngquant.tar.gz', import.meta.url));
 
