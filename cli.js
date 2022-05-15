9931869230af44e977cec4a03d84a4e77c4dcd5a
Dave Shifflett <dave@corpulent.coffee>
2022-05-15 08:13:37 -0500

    Fix execa import for CLI entrypoint (#139)

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import process from 'node:process';
-import execa from 'execa';
+import {execa} from 'execa';
 import pngquant from './index.js';
 
 execa(pngquant, process.argv.slice(2), {stdio: 'inherit'});
